f1daf6b6e5ede6ff197656928c76b64d838f83d9
Rohan Chawla <rohanchawla@Rohans-MacBook-Air.local>
2020-07-10 17:08:59 +0530

    try 5

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const router = express.Router();
-const Todo = require('../models/todo');
+const User = require('../models/user');
 const passport = require('passport');
 const middleware = require('../middleware');
 //!login etc
